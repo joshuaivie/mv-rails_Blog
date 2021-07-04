b2a52b2a8c06d9c6f8fe2843379971314e9fb5ab
joshuaivie <joshua@lifeonline.com.ng>
2021-07-04 19:36:47 +0100

    add authentication

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,4 +1,6 @@
 class ArticlesController < ApplicationController
+  http_basic_authenticate_with name: 'Auth User', password: 'Auth Secret', except: %i[index show]
+
   def index
     @articles = Article.all
   end
--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,4 +1,5 @@
 class CommentsController < ApplicationController
+  http_basic_authenticate_with name: 'Auth User', password: 'Auth Secret', only: :destory
   def create
     @article = Article.find(params[:article_id])
     @comment = @article.comments.create(comment_params)
